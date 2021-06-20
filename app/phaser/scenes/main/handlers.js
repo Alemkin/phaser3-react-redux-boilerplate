07607c85811d38e33163dad30306eba8048dbc52
Alexander Lemkin <alexander_lemkin@carmax.com>
2021-06-20 11:01:48 -0400

    Commenting out some unused assets for now

--- app/phaser/scenes/main/handlers.js
+++ app/phaser/scenes/main/handlers.js
@@ -24,21 +24,21 @@ function create (game) {
   // game.add.image(0, 0, Assets.BACKGROUND2).setOrigin(0)
   playerController.setPlayerAnimations(game)
 
-  const map = game.make.tilemap({ key: Assets.TUXEMON_JSON })
+  game.map = game.make.tilemap({ key: Assets.TUXEMON_JSON })
 
-  const tileset = map.addTilesetImage('tuxmon-sample-32px-extruded', Assets.TUXEMON)
+  const tileset = game.map.addTilesetImage('tuxmon-sample-32px-extruded', Assets.TUXEMON)
 
-  const belowLayer = map.createStaticLayer('Below Player', tileset, 0, 0)
-  const worldLayer = map.createStaticLayer('World', tileset, 0, 0)
-  const aboveLayer = map.createStaticLayer('Above Player', tileset, 0, 0)
+  const belowLayer = game.map.createStaticLayer('Below Player', tileset, 0, 0)
+  const worldLayer = game.map.createStaticLayer('World', tileset, 0, 0)
+  const aboveLayer = game.map.createStaticLayer('Above Player', tileset, 0, 0)
 
   worldLayer.setCollisionByProperty({ collides: true })
   aboveLayer.setDepth(10)
 
-  const spawnPoint = map.findObject('Objects', obj => obj.name === 'Spawn Point')
+  const spawnPoint = game.map.findObject('Objects', obj => obj.name === 'Spawn Point')
 
-  game.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
-  game.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+  game.cameras.main.setBounds(0, 0, game.map.widthInPixels, game.map.heightInPixels)
+  game.physics.world.setBounds(0, 0, game.map.widthInPixels, game.map.heightInPixels)
 
   game.cursors = game.input.keyboard.createCursorKeys()
 
--- app/phaser/scenes/preload/index.js
+++ app/phaser/scenes/preload/index.js
@@ -1,9 +1,9 @@
 import Phaser from 'phaser'
 import { PRELOAD, MAIN } from '../sceneNames'
 import Assets from '../assets'
-import dockImg from '../../../../assets/dock.png'
+// import dockImg from '../../../../assets/dock.png'
 import playerImg from '../../../../assets/fmship.png'
-import victorianImg from '../../../../assets/victorian-preview.png'
+// import victorianImg from '../../../../assets/victorian-preview.png'
 import tuxemonImg from '../../../../assets/tuxemon/tuxmon-sample-32px-extruded.png'
 import tuxemonJson from '../../../../assets/tuxemon/tuxemon-town.json'
 import gentlemanImg from '../../../../assets/gentleman.png'
@@ -14,8 +14,8 @@ class PreloadScene extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image(Assets.BACKGROUND, dockImg)
-    this.load.image(Assets.BACKGROUND2, victorianImg)
+    // this.load.image(Assets.BACKGROUND, dockImg)
+    // this.load.image(Assets.BACKGROUND2, victorianImg)
     this.load.image(Assets.PLAYER, playerImg)
 
     this.load.image(Assets.TUXEMON, tuxemonImg)
